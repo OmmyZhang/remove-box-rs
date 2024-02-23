63add997eef30d32326ec95e3f4a1422565533f2
Tdxdxoz <tdxdxoz@gmail.com>
2024-02-23 20:15:34 +0800

    create table if not exists

--- be/schema.sql
+++ be/schema.sql
@@ -1,10 +1,8 @@
-DROP TABLE IF EXISTS records;
-
-CREATE TABLE records (
+CREATE TABLE IF NOT EXISTS records (
   id serial PRIMARY KEY,
   name varchar(50) NOT NULL UNIQUE,
   score INTEGER NOT NULL,
   time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE INDEX idx_score_time ON records (score, time);
+CREATE INDEX IF NOT EXISTS idx_score_time ON records (score, time);
