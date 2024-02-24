11e8d5f0aebb61f06a01f0c0f2d8f12b53a07690
Tdxdxoz <tdxdxoz@gmail.com>
2024-02-24 22:14:47 +0800

    fix: use shuttle_secrets

--- be/src/main.rs
+++ be/src/main.rs
@@ -3,6 +3,7 @@ use actix_web::web::{scope, Data, ServiceConfig};
 use ring::hmac;
 use shuttle_actix_web::ShuttleActixWeb;
 use shuttle_runtime::CustomError;
+use shuttle_secrets::SecretStore;
 use sqlx::{Executor, PgPool};
 
 mod record;
@@ -11,18 +12,20 @@ mod sig;
 #[shuttle_runtime::main]
 async fn main(
     #[shuttle_shared_db::Postgres] pool: PgPool,
+    #[shuttle_secrets::Secrets] secret_store: SecretStore,
 ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     pool.execute(include_str!("../schema.sql"))
         .await
         .map_err(CustomError::new)?;
+
+    let key_value = secret_store.get("KEY_VALUE").unwrap();
+    let key = hmac::Key::new(hmac::HMAC_SHA256, key_value.as_bytes());
 
     let config = move |cfg: &mut ServiceConfig| {
         let cors = Cors::default()
             .allow_any_origin()
             .allowed_methods(["GET", "POST"])
             .allowed_headers(["content-type", "sig", "sig-t"]);
-        let key_value = std::env::var("KEY_VALUE").unwrap();
-        let key = hmac::Key::new(hmac::HMAC_SHA256, key_value.as_bytes());
         cfg.service(
             scope("/api/record")
                 .wrap(cors)
